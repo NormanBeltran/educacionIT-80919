129c55d5314280c6dfb217d4ba5348d179f0ecbc
NormanBeltran <josenormanbeltran@gmail.com>
2026-01-29 20:51:58 -0300

    Clase 3

--- mip/miapp/urls.py
+++ mip/miapp/urls.py
@@ -2,12 +2,18 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("",            views.index,        name="index"),
-    path("acerca/",     views.acerca,       name="acerca"),
-    path("contacto/",   views.contacto,     name="contacto"),
+    path("",                views.index,        name="index"),
+    path("acerca/",         views.acerca,       name="acerca"),
+    path("contacto/",       views.contacto,     name="contacto"),
 
-    path("cursos/",     views.cursos,       name="cursos"),
+    path("cursos/",         views.cursos,       name="cursos"),
 
-    path("aero/",       views.aero,         name="aero"),
-    path("aero_api/",   views.aero_api,     name="aero_api"),
+    path("aero/",           views.aero,         name="aero"),
+    path("aero_api/",       views.aero_api,     name="aero_api"),
+
+    path("bienvenido/",     views.bienvenido,   name="bienvenido"),
+    path("bienvenido2/",    views.bienvenido2,  name="bienvenido2s"),
+
+    path("uncurso/<id>/",   views.unCurso,      name="uncurso"),
+    path("allcursos/",      views.allCursos,    name="allcursor"),
 ]
